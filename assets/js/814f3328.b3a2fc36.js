3daa61bb553e2a797b2cbfcb2e5ba55e20af6a1e
boage <www.yuehaibo@126.com>
2022-08-04 09:24:23 +0800

    Deploy website - based on fd44b6a97c7c5922447b891cd29d6f62e40a5a48

---
+++ assets/js/814f3328.b3a2fc36.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_documents=self.webpackChunkmy_documents||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"\u4e2a\u4eba\u4fe1\u606f","permalink":"/my-documents/blog/\u4e2a\u4eba\u7b80\u4ecb"},{"title":"Welcome","permalink":"/my-documents/blog/welcome"}]}')}}]);
